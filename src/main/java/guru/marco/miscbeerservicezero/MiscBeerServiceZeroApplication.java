626ab2b574bc09379850651bd279000f0b8ce645
Marco Giacomello <m.giacomello@insiel.it>
2022-11-11 23:36:53 +0100

    @wip

---
+++ src/main/java/guru/marco/miscbeerservicezero/MiscBeerServiceZeroApplication.java
@@ -0,0 +1,13 @@
+package guru.marco.miscbeerservicezero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiscBeerServiceZeroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiscBeerServiceZeroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/marco/miscbeerservicezero/MiscBeerServiceZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.marco.miscbeerservicezero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiscBeerServiceZeroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
